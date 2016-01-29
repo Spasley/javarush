171612445f4a1a63403e5db641fee4618d0269b9
Болодя <proff.spasley@gmail.com>
2016-01-29 23:10:43 +0600

    level18.lesson10.home07

---
+++ src/com/javarush/test/level18/lesson10/home07/Solution.java
@@ -0,0 +1,53 @@
+package com.javarush.test.level18.lesson10.home07;
+
+/* Поиск данных внутри файла
+Считать с консоли имя файла
+Найти в файле информацию, которая относится к заданному id, и вывести ее на экран в виде, в котором она записана в файле.
+Программа запускается с одним параметром: id (int)
+Закрыть потоки. Не использовать try-with-resources
+
+В файле данные разделены пробелом и хранятся в следующей последовательности:
+id productName price quantity
+
+где id - int
+productName - название товара, может содержать пробелы, String
+price - цена, double
+quantity - количество, int
+
+Информация по каждому товару хранится в отдельной строке
+*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //args = new String[1];
+        //args[0] = "12";
+        int parameter = Integer.parseInt(args[0]);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = null;
+        String result = null;
+        int id = 0;
+        try {
+            fileName = reader.readLine();
+            Scanner read = new Scanner(new File(fileName));
+            while (read.hasNext()) {
+                id = Integer.parseInt(read.next());
+                if (id == parameter) {
+                    result = read.nextLine();
+                }
+                else {
+                    read.nextLine();
+                }
+            }
+            System.out.println(parameter + result);
+            read.close();
+            reader.close();
+        }
+        catch (IOException e) {}
+    }
+}
