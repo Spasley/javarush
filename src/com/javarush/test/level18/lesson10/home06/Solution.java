ca185f1833e4d2f34447467e59e055215aa2a3f0
kolenov <kolenov@trialpay.com>
2016-02-04 14:17:49 +0600

    level18.lesson10.home06 чуть поколдовал, ничего не сделал, по-прежнему не принимает

--- src/com/javarush/test/level18/lesson10/home06/Solution.java
+++ src/com/javarush/test/level18/lesson10/home06/Solution.java
@@ -21,7 +21,6 @@ package com.javarush.test.level18.lesson10.home06;
 f 361
 */
 
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
@@ -30,7 +29,7 @@ import java.util.TreeMap;
 public class Solution {
     public static void main(String[] args) {
         args = new String[1];
-        args[0] = "d:\\data.txt";
+        args[0] = "/home/volodka/data";
         String filename = args[0];
         TreeMap<Byte, Integer> result = new TreeMap();
         try
